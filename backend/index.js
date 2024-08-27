b4c99020ed7ad45cfe1dddb5717818acc63a27f7
Nayan7652Thakur <nayanthakur7652@gmail.com>
2024-08-27 20:01:09 +0530

    my code

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express, { urlencoded } from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import connectDB from './utils/db.js'
+dotenv.config({})
+const PORT = process.env.PORT || 3000;
+
+
+const app = express()
+
+app.get("/", (req,res) => {
+    return res.status(200).json({
+        message:"Im' coming from backend",
+      success:true   
+    })
+})
+
+//widdlewares
+
+app.use(express.json())
+app.use(cookieParser());
+app.use(urlencoded({extended:true}))
+const corsOption = {
+    origin:'http://localhost:5173',
+    credentials: true
+}
+
+app.use(cors(corsOption))
+
+
+app.listen(PORT, () =>{
+    connectDB()
+    console.log(`server listen at post ${PORT}`);
+})
