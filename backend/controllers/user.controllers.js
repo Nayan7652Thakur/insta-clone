b3ea8d3a542e55f1a3e74d3171fb60c55a972fe2
Nayan7652Thakur <nayanthakur7652@gmail.com>
2024-08-28 12:23:18 +0530

    user.controllers

---
+++ backend/controllers/user.controllers.js
@@ -0,0 +1,124 @@
+import { User } from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export const register = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "All fields are required",
+                success: false
+            });
+        }
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({
+                message: "Email is already in use. Try a different email.",
+                success: false
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        return res.status(201).json({
+            message: "Account created successfully!",
+            success: true
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Server error",
+            success: false
+        });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Please fill in all fields",
+                success: false
+            });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Incorrect email or password",
+                success: false
+            });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordMatch) {
+            return res.status(401).json({
+                message: "Incorrect email or password",
+                success: false
+            });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1d' });
+
+        return res.cookie('token', token, {
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 31 * 24 * 60 * 1000
+        }).json({
+            message: `Welcome back, ${user.username}`,
+            success: true,
+            user: {
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+                profilePicture: user.profilePicture,
+                bio: user.bio,
+                followers: user.followers,
+                following: user.following,
+                posts: user.posts
+            }
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Server error",
+            success: false
+        });
+    }
+};
+
+
+export const logout = async (_, res) => {
+    try {
+        return res.cookie("token", "", { maxAge: 0 }).json({
+            message: 'user logout',
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getProfile = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        let user = await User.findById(userId)
+
+    } catch (error) {
+        console.log(error);
+    }
+}
