a4fcf395099b97ef0a380c8388f15e4d2cb00267
Nayan7652Thakur <nayanthakur7652@gmail.com>
2024-09-02 14:22:04 +0530

    afternoon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Signup from './components/Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className='border-red-700 border-7 underline'>dvsvdf</p>
+    <Signup />
     </>
   )
 }
