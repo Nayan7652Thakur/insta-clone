ac941d47e7800b5de69cbb85af5fd8c55bf16958
Nayan7652Thakur <nayanthakur7652@gmail.com>
2024-09-04 21:14:45 +0530

    night

---
+++ frontend/src/components/CommentDialog.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Dialog, DialogContent } from './ui/dialog'
+
+const CommentDialog = () => {
+  return (
+    <Dialog>
+        <DialogContent>
+            <img src="" alt="" />
+        </DialogContent>
+    </Dialog>
+  )
+}
+
+export default CommentDialog
