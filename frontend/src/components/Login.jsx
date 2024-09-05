945f7c6a01472b0c00c7d5d81f57a7b2154a487c
Nayan7652Thakur <nayanthakur7652@gmail.com>
2024-09-05 19:27:42 +0530

    redux

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,6 +5,7 @@ import { Button } from './ui/button';
 import { toast } from 'sonner';
 import { Link, useNavigate } from 'react-router-dom';
 import { Loader2 } from 'lucide-react';
+import { useDispatch } from 'react-redux';
 
 const Login = () => {
     const [input, setInput] = useState({
@@ -15,6 +16,7 @@ const Login = () => {
     const [loading, setLoading] = useState(false)
 
     const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     const changeEventHandler = (e) => {
         setInput({ ...input, [e.target.name]: e.target.value });
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,11 @@ import App from './App.jsx'
 import './index.css'
 import { Toaster } from './components/ui/sonner.jsx'
 import { Provider } from 'react-redux'
+import store from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
+    <Provider store={store}>
     <App />
     <Toaster />
     </Provider>
